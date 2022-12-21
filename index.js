f2e8edd4b502bbc02bf03b0290009f17d3db935a
Isaac_Talb <89933458+IsaacTalb@users.noreply.github.com>
2022-12-21 22:55:41 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "yeah, it's worked";
 }
+
+var conn = MongoClient.connect('mongodb://https://github.com/IsaacTalb/duckcloud.info/') // returns a Promise
+
+app.get('/', function(req, res) {
+    conn.then(client=> client.db('test').collection('test').find({}).toArray(function(err, docs) {
+        if(err) { console.error(err) }
+        res.send(JSON.stringify(docs))
+    }))
+})
