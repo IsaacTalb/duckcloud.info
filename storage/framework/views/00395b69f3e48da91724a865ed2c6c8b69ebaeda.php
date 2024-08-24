4edacfd0fd464486c64b7446bdc350e9c3ddbd3f
Isaac-Talb <thaungahlaybaw@gmail.com>
2024-08-24 17:53:34 +0630

    project init

--- storage/framework/views/00395b69f3e48da91724a865ed2c6c8b69ebaeda.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH D:\Duck_Cloud_Folders\Duck Cloud Website\duckcloud.info\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/3552009419c2c9d7605daa10de17332b9dc61f7f.php
+++
@@ -1,9 +0,0 @@
-<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td>
-<?php echo e(Illuminate\Mail\Markdown::parse($slot)); ?>
-
-</td>
-</tr>
-</table>
-<?php /**PATH D:\Duck_Cloud_Folders\Duck Cloud Website\duckcloud.info\vendor\laravel\framework\src\Illuminate\Mail/resources/views/html/subcopy.blade.php ENDPATH**/ ?>
