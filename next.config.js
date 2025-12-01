e80ef5017d4ec52f40e3464f7cd3a4534c6901d4
IsaacTalb <thaungahlaybaw@gmail.com>
2025-12-02 06:08:07 +0630

    update for maintainence options

--- next.config.js
+++ next.config.js
@@ -5,26 +5,32 @@ const nextConfig = {
   images: {
     formats: ['image/webp', 'image/avif'],
   },
+
+  // Security Headers
   headers: async () => {
     return [
       {
         source: '/:path*',
         headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
         ],
       },
     ];
+  },
+
+  // Maintenance Mode Redirect
+  async redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? {
+            source: "/((?!maintenance).*)", // redirect everything except maintenance
+            destination: "/maintenance.html",
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean);
   },
 };
 
